8c9a11c91c51f78b76066e63860798f047fb331d
Przemyslaw Kaleta <przemek2889@gmail.com>
2020-04-18 19:55:23 +0200

    Lr scheduler during training.

--- train_resnet.py
+++ train_resnet.py
@@ -1,51 +1,32 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
-import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
-import numpy as np
 from tqdm import tqdm, trange
-from typing import List
-from collections import Counter
-import cv2
 from datetime import datetime
 
 from data import get_loader_splits
-
+from utils import get_trainable, validate, lr_schedule
 
 torch.manual_seed(42)
 
-
-trainloader, validloader = get_loader_splits()
-
+trainloader, validloader = get_loader_splits(augment=True)
 
 net = torchvision.models.resnet18(pretrained=False, progress=True)
-print("Loaded model")
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 # Moving network parameters to device
 net.to(device)
 # Tensorboard
-writer = SummaryWriter('runs/cifar/transfer')
+writer = SummaryWriter('runs/cifar/resnet')
+# Optimization
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(get_trainable(net.parameters()), lr=0.001, weight_decay=0.005)
+optimizer = optim.Adam(get_trainable(net.parameters()), lr=0.001, weight_decay=0.005)
+scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_schedule)
 
-def validate(loader):
-    running_loss = 0.0
-    n_correct = 0
-    with torch.no_grad():
-        for i, (x, y) in enumerate(loader):
-            x, y = x.to(device), y.to(device)
-            y_pred = net(x)
-            running_loss += criterion(y_pred, y)
-            n_correct += (torch.argmax(y_pred, axis=1) == y).sum().int().item()
-    return running_loss / len(loader), n_correct / (len(loader)*loader.batch_size)
-
-        
-
+# Training loop
 N_EPOCHS = 70
-for epoch in range(N_EPOCHS):  
+for epoch in range(N_EPOCHS):
     running_loss = 0.0
     n_correct = 0
     print("Epoch {} / {}".format(epoch + 1, N_EPOCHS))
@@ -62,61 +43,31 @@ for epoch in range(N_EPOCHS):
         running_loss += loss.item()
         with torch.no_grad():
             n_correct += (torch.argmax(y_pred, axis=1) == y).sum().int().item()
-        
-        if i % 1000 == 999:    # every 1000 mini-batches...
+
+        if i % 1000 == 999:  # every 1000 mini-batches...
             step = epoch * len(trainloader) + i
-            valid_loss, valid_accuracy = validate(validloader)
-            train_accuracy = n_correct / (1000*trainloader.batch_size)
+            valid_loss, valid_accuracy = validate(net, criterion, validloader, device)
+            train_accuracy = n_correct / (1000 * trainloader.batch_size)
 
             # ...log the running loss
-            writer.add_scalar('training loss',
-                            running_loss / 1000,
-                            step)
-            writer.add_scalar('training accuracy',
-                            train_accuracy,
-                            step)
-            writer.add_scalar('validation loss',
-                            valid_loss,
-                            step)
-            writer.add_scalar('validation accuracy',
-                            valid_accuracy,
-                            step)
+            writer.add_scalar('training loss', running_loss / 1000, step)
+            writer.add_scalar('training accuracy', train_accuracy, step)
+            writer.add_scalar('validation loss', valid_loss, step)
+            writer.add_scalar('validation accuracy', valid_accuracy, step)
 
-            print("Training loss: {}, training accuracy: {}, validation loss: {}, validation accuracy: {}"\
-                    .format(
-                        running_loss / 1000, 
-                        train_accuracy,
-                        valid_loss,
-                        valid_accuracy)
-                    )
+            print("Training loss: {}, training accuracy: {}, validation loss: {}, validation accuracy: {}"
+                  .format(running_loss / 1000, train_accuracy, valid_loss, valid_accuracy))
 
             running_loss = 0.0
             n_correct = 0
     # Saving model parameters
     torch.save(net.state_dict(), "states/epoch{}".format(epoch))
+    scheduler.step()
 
 writer.add_graph(net, x)
 
+_, acc = validate(net, criterion, validloader, device)
+print("Evaluation accuracy", acc)
 
-def get_accuracy_and_predictions(data):
-    predictions = []
-    n = len(data)
-    correct = 0
-    for tensor, label in tqdm(data, leave=False):
-        tensor = tensor.to(device)
-        with torch.no_grad():
-            logits = net(tensor.view(-1, 3, 32, 32))
-            y_pred = logits.argmax().item()
-            predictions.append(y_pred)
-            if (y_pred == label):
-                correct += 1
-    return correct / n, predictions
-
-
-acc, preds = get_accuracy_and_predictions(testset)
-print("Accuracy", acc)
-print(Counter(preds))
-
-acc, preds = get_accuracy_and_predictions(trainset)
+_, acc = validate(net, criterion, trainloader, device)
 print("Training accuracy", acc)
-print(Counter(preds))
--- utils.py
+++ utils.py
@@ -1,5 +1,19 @@
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def lr_schedule(epoch: int) -> float:
+    if epoch < 20:
+        return 0.1
+    elif epoch < 30:
+        return 0.05
+    elif epoch < 50:
+        return 0.01
+    elif epoch < 70:
+        return 0.001
+    else:
+        return 0.0003
 
 
 def imshow(tensor, ax=None):
@@ -17,3 +31,15 @@ def imshow(tensor, ax=None):
 
 def get_trainable(parameters):
     return (p for p in parameters if p.requires_grad)
+
+
+def validate(net, criterion, loader, device):
+    running_loss = 0.0
+    n_correct = 0
+    with torch.no_grad():
+        for i, (x, y) in enumerate(loader):
+            x, y = x.to(device), y.to(device)
+            y_pred = net(x)
+            running_loss += criterion(y_pred, y)
+            n_correct += (torch.argmax(y_pred, axis=1) == y).sum().int().item()
+    return running_loss / len(loader), n_correct / (len(loader)*loader.batch_size)
