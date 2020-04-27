0b089e91c91679feea29e8ec252f92ee606b11cc
Przemyslaw Kaleta <przemek2889@gmail.com>
2020-04-27 07:13:58 +0200

    Added script to evaluate kaggle dataset.

--- data.py
+++ data.py
@@ -1,27 +1,34 @@
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import torchvision
 import torchvision.transforms as transforms
+import os
+from PIL import Image
+from tqdm import tqdm
+
+
+stats = ((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+
+to_tensor_normalize = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize(*stats)
+    ]
+)
+
+augment_transform = transforms.Compose(
+          [
+              transforms.RandomHorizontalFlip(p=0.5),
+              transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
+              transforms.RandomAffine((-10, 10)),
+              transforms.RandomCrop(32, padding=4),
+              to_tensor_normalize
+          ]
+      )
 
 
 def get_loader_splits(batch_size: int = 64, valid_batch_size: int = 128,
                       augment: bool = True, augment_valid: bool = False):
-    stats = ((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-
-    to_tensor_normalize = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize(*stats)
-        ]
-    )
-
-    augment_transform = transforms.Compose(
-        [
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomCrop(32, padding=4),
-            to_tensor_normalize
-        ]
-    )
 
     train_transform = augment_transform if augment else to_tensor_normalize
 
@@ -35,9 +42,35 @@ def get_loader_splits(batch_size: int = 64, valid_batch_size: int = 128,
                                            download=True, transform=valid_transform)
 
     # Trainloader
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+    trainloader = DataLoader(trainset, batch_size=batch_size,
                                               shuffle=True, num_workers=2)
 
-    validloader = torch.utils.data.DataLoader(testset, batch_size=valid_batch_size, shuffle=True, num_workers=2)
+    validloader = DataLoader(testset, batch_size=valid_batch_size, shuffle=True, num_workers=2)
 
     return trainloader, validloader
+
+
+class CifarKaggleTestset(Dataset):
+    def __init__(self, data_root, augment: bool = True):
+        self.augment = augment
+        self.samples = []
+        for image_name in tqdm(os.listdir(data_root)):
+            image_path = os.path.join(data_root, image_name)
+            with open(image_path, 'r') as f:
+                img = Image.open(image_path)
+                self.samples.append((img.convert('RGB'), int(image_name[:-4])))
+                img.close()
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        if self.augment:
+            return augment_transform(self.samples[idx])
+        else:
+            return to_tensor_normalize(self.samples[idx])
+
+
+def get_kaggle_testloader(data_root: str, augment: bool = True, batch_size: int = 128):
+    testset = CifarKaggleTestset(data_root, augment=augment)
+    return DataLoader(testset, batch_size=batch_size)
---
+++ evaluate.py
@@ -0,0 +1,63 @@
+"""
+Script to evaluate ensemble of WideResNet models created by augmenting test data.
+Each predictions is made multiple times on randomly modified images. Then, the average of all
+predictions is taken as a final prediction.
+"""
+
+import torch
+import argparse
+import pandas as pd
+import matplotlib.pyplot as plt; plt.style.use("fivethirtyeight")
+from tqdm import trange
+
+from data import get_kaggle_testloader
+from models import WideResNet22
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--path", type=str, required=True, help="Path to the Kaggle test folder.")
+parser.add_argument("-s", "--state_path", type=str, required=True)
+parser.add_argument("--augment", action="store_true", help="Whether to use data augmentation.")
+parser.add_argument("-e", "--ensemble", type=int, default=1, help="Number of ensembles to use")
+parser.add_argument("--save_path", type=str, default="submission.csv", help="Path in which to save submission.")
+parser.add_argument("-bs", "--batch_size", type=int, default=1000, help="Test batch size.")
+args = parser.parse_args()
+print(args)
+
+classes = ('airplane', 'automobile', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+testloader = get_kaggle_testloader(args.path, augment=args.augment, batch_size=args.batch_size)
+
+device = torch.device("cuda")
+
+# Reading net
+net = WideResNet22()
+net.load_state_dict(torch.load(args.state_path, map_location=device))
+net.to(device).eval()
+
+# Making predictions
+ensemble_preds = []
+indexes = None
+for _ in trange(args.ensemble):
+    indexes = []
+    preds = []
+    n_correct = 0
+    total = 0
+    with torch.no_grad():
+        for i, (x, idx) in enumerate(testloader):
+            x = x.to(device)
+            y_pred = net(x)
+            preds.append(y_pred)
+            indexes.append(idx)
+    y_pred = torch.cat(preds)
+    ensemble_preds.append(y_pred)
+
+mean_pred = torch.mean(torch.stack(ensemble_preds), 0)
+final_prediction = [classes[label.item()] for label in torch.argmax(mean_pred, 1)]
+
+# Generating submission
+submission = pd.DataFrame({"id": indexes, "label": final_prediction})
+submission.sort_values("id")
+print(submission.head())
+submission.to_csv(args.save_path, index=False)
